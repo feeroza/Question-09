895136d103757279abd131d7fc3f4127959f734b
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 21:59:57 +0500

    Add files via upload

---
+++ Question 9/009.js
@@ -0,0 +1,5 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create
+// a message that reveals your favorite number. Print that message.
+//favorite number.
+var favorite = 9;
+console.log("my favorit number is ".concat(favorite));
---
+++ Question 9/009.ts
@@ -0,0 +1,8 @@
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create
+// a message that reveals your favorite number. Print that message.
+
+
+
+//favorite number.
+let favorite: number = 9;
+console.log(`my favorit number is ${favorite}`)
